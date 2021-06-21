767a7368afaddea4dc4609ae878803e7cb59688a
mmirwaldt <michael.mirwaldt@gmail.com>
2021-06-21 09:01:02 +0200

    return int values instead of null

--- src/main/java/net/mirwaldt/bug/DummyInterpreter.java
+++ src/main/java/net/mirwaldt/bug/DummyInterpreter.java
@@ -3,12 +3,12 @@ package net.mirwaldt.bug;
 public class DummyInterpreter implements Interpreter {
     @Override
     public <VarType extends Variable> Object query(OneVariableQuery<VarType> query) {
-        return null;
+        return 1; // we don't return null
     }
 
     @Override
     public <Var1Type extends Variable, Var2Type extends Variable> Object
     query(TwoVariablesQuery<Var1Type, Var2Type> query) {
-        return null;
+        return 2; // we don't return null
     }
 }
